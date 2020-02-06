14e4ef2e1a043b4937c5fa8a5473c347a52c87e9
David C <davidc@gamehouse.com>
2020-02-06 18:09:23 +0100

    Fix to use ~/.kube/config instead of /root/.kube/config

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,6 @@ export AWS_SECRET_ACCESS_KEY=$2
 export AWS_DEFAULT_REGION=$3
 export K8S_CLUSTER=$4
 
-export
-
 aws eks update-kubeconfig --name $4
-cp /root/.kube/config ./kubeconfig
-cat /root/.kube/config
+cp ~/.kube/config ./kubeconfig
+cat ~/.kube/config
